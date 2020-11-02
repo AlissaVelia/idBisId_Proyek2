93ead1b73dcc287d7c0165658a49e802dbaac75c
hananursa <salsabillahhana@gmail.com>
2020-11-02 20:49:11 +0700

    front end admin

--- application/controllers/admin_ide.php
+++ application/controllers/admin_ide.php
@@ -16,8 +16,25 @@ class admin_ide extends CI_Controller {
     public function index()
     {
         $data['title'] = 'Dashboard - idBisid';
-        $this->load->view('template/admin/header_admin',$data);
+        $this->load->view('template/admin_ide/header_adminide',$data);
+        $this->load->view('template/admin_ide/sidebar_adminide');
         $this->load->view('admin_ide/index',$data);
+        // $this->load->view('template/admin/footer_admin');
+    }
+    public function ide_bisnis()
+    {
+        $data['title'] = 'Dashboard - idBisid';
+        $this->load->view('template/admin_ide/header_adminide',$data);
+        $this->load->view('template/admin_ide/sidebar_adminide',$data);
+        $this->load->view('admin_ide/ide_bisnis',$data);
+        // $this->load->view('template/admin/footer_admin');
+    }
+    public function grafik_idbis()
+    {
+        $data['title'] = 'Dashboard - idBisid';
+        $this->load->view('template/admin_ide/header_adminide',$data);
+        $this->load->view('template/admin_ide/sidebar_adminide',$data);
+        $this->load->view('admin_ide/grafik_idbis',$data);
         // $this->load->view('template/admin/footer_admin');
     }
 }
--- application/controllers/admin_lembaga.php
+++ application/controllers/admin_lembaga.php
@@ -16,18 +16,35 @@ class admin_lembaga extends CI_Controller {
     public function index()
     {
         $data['title'] = 'Dashboard - idBisid';
-        $this->load->view('template/admin/header_admin',$data);
+        $this->load->view('template/admin_lembaga/header_adminlembaga',$data);
+        $this->load->view('template/admin_lembaga/sidebar_adminlembaga',$data);
         $this->load->view('admin_lembaga/index',$data);
         // $this->load->view('template/admin/footer_admin');
     }
-
-    // public function data_lembaga()
-    // {
-    //     $data['title'] = 'Data Lembaga - idBisid';
-    //     $this->load->view('template/admin/header_admin',$data);
-    //     $this->load->view('adminlembaga/data_lembaga',$data);
+    public function data_lembaga()
+    {
+        $data['title'] = 'Dashboard - idBisid';
+        $this->load->view('template/admin_lembaga/header_adminlembaga',$data);
+        $this->load->view('template/admin_lembaga/sidebar_adminlembaga',$data);
+        $this->load->view('admin_lembaga/data_lembaga',$data);
         // $this->load->view('template/admin/footer_admin');
-    // }
+    }
+    public function lembaga_favorite()
+    {
+        $data['title'] = 'Dashboard - idBisid';
+        $this->load->view('template/admin_lembaga/header_adminlembaga',$data);
+        $this->load->view('template/admin_lembaga/sidebar_adminlembaga',$data);
+        $this->load->view('admin_lembaga/lembaga_favorite',$data);
+        // $this->load->view('template/admin/footer_admin');
+    }
+    public function grafik_lembaga()
+    {
+        $data['title'] = 'Dashboard - idBisid';
+        $this->load->view('template/admin_lembaga/header_adminlembaga',$data);
+        $this->load->view('template/admin_lembaga/sidebar_adminlembaga',$data);
+        $this->load->view('admin_lembaga/grafik_lembaga',$data);
+        // $this->load->view('template/admin/footer_admin');
+    }
 
 }
 
